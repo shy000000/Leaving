a30661a83e01260276e0f300332441ee9878e0ca
shy000000 <404380366@qq.com>
2016-09-01 10:09:37 +0800

    first commit

---
+++ src/main/java/com/apache/controller/MainController.java
@@ -0,0 +1,16 @@
+package com.apache.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+/**
+ * Created by shy on 2016/9/1.
+ */
+@Controller
+public class MainController {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "index";
+    }
+}
